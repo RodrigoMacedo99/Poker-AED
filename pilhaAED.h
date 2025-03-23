81dfa86fd8cbb42f50c2579ce45505e9793e5f20
amorimjoaopedro <joaopedroamorim13@gmail.com>
2025-03-23 09:25:20 -0300

    Update pilhaAED.h

--- pilhaAED.h
+++ pilhaAED.h
@@ -4,116 +4,82 @@
 #define MAX 100
 
 typedef int
-	tp_item;
-	
-typedef struct 
-{
-	int topo;
-	tp_item item[MAX];	
+tp_item;
+
+typedef struct {
+int topo;
+tp_item item[MAX];
 } tp_pilha;
 
-//Função respondavel por incializar a pilha, atribuindo -1 ao topo.
-void InicializaPilha(tp_pilha *p)
-{
-	p->topo = -1;
+//inicializa a pilha atribuindo valor -1 ao topo
+void inicializaPilha(tp_pilha *p){
+p->topo = -1;
 }
 
-//Função responsavel por verificar se a pilha esta vazia, vendo se o valor atualmente atribuido ao topo é -1.
-int PilhaVazia(tp_pilha *p)
-{
-	if (p->topo == -1)
-	{
-		return 1;
-	}
-	
-	else
-	{
-		return 0;
-	}
+//verifica se a pilha estÃ¡ vazaia. Em caso positivo retorna 1 (verdade)
+//caso contrÃ¡rio, retorna 0 (falso)
+
+int pilhaVazia(tp_pilha *p){
+if (p->topo == -1) return 1;
+return 0;
 }
 
-//Função responsavel por verificar se a pilha esta vazia, vendo se o valor atualmente atribuido ao topo é igual a MAX-1.
-int PilhaCheia(tp_pilha *p)
-{
-	if (p->topo == MAX-1)
-	{
-		return 1;
-	}
-	
-	else
-	{
-		return 0;
-	}
+//verifica se a pilha estÃ¡ cheia . Em caso positivo retorna 1 (verdade)
+//caso contrÃ¡rio, retorna 0 (falso)
+
+int pilhaCheia(tp_pilha *p){
+if (p->topo == MAX-1) {
+return 1;
+} else {
+return 0;
+}
 }
 
-//Função responsavel por adicionar um elemento ao topo da pilha.
-int Empilhar(tp_pilha *p, tp_item e)
-{
-	if(PilhaCheia(p) == 1)
-	{
-		return 0;
-	}
-	
-	else
-	{
-		p->topo++;
-		p->item[p->topo] = e;
-		
-		return 1;
-	}
+//Tenta empilhar um elemento e caso nÃ£o consiga (pilha cheia), ela retorna  0 (falha).
+//Caso consiga, retorna 1 (sucesso)
+
+int push(tp_pilha *p, tp_item e){
+if (pilhaCheia(p)==1) return 0;
+p->topo++;
+p->item[p->topo]=e;
+return 1;
 }
 
-//Função responsavel por remover um elemento do topo da pilha.
-int Desempilhar(tp_pilha *p, tp_item *e)
-{
-	if (PilhaVazia(p) == 1)
-	{
-		return 0;
-	}
-	
-	else
-	{
-		*e = p->item[p->topo];
-		p->topo--;
-		
-		return 1;
-	}
+//Tenta desempilha um elemento e caso nÃ£o consiga (pilha vazia), retorna 0 (falha)
+//Caso consiga, retorna 1 (sucesso)
+
+int pop(tp_pilha *p, tp_item *e){
+if (pilhaVazia(p)==1) return 0;
+*e=p->item[p->topo];
+p->topo--;
+return 1;
 }
 
-//Função responsavel por verificar o valor do elemento no topo da pilha e retornar este valor.
-int VerificarTopo(tp_pilha *p, tp_item *e)
-{
-	if (PilhaVazia(p) == 1)
-	{
-		return 0;
-	}
-	
-	else
-	{
-		*e = p->item[p->topo];
-		
-		return 1;
-	}
+//Tenta apresentar o elemento do topo da pilha e caso nÃ£o consiga (pilha vazia), retorna 0 (falha)
+//Caso consiga, retorna 1 (sucesso)
+
+int top(tp_pilha *p, tp_item *e){
+if (pilhaVazia(p)) return 0;
+*e=p->item[p->topo];
+return 1;
 }
 
-//Função responsavel por imprimir os valores dos elementos da pilha.
-void ImprimePilha(tp_pilha p)
-{
-	tp_item e;
-	
-	while (!PilhaVazia(&p))
-	{
-		Desempilhar(&p, &e);
-		
-		printf("%d\n", e);
-	}
+//desempilha todos os elementos (um por um) e imprime em tela
+
+void imprimePilha(tp_pilha p){
+tp_item e;
+printf("\n");
+while (!pilhaVazia(&p)){
+pop(&p, &e);
+printf("%d ",e);
+}
 }
 
+//Retorna a quantidade de elementos empilhados
 
-//Função responsavel por verificar em qual posição o topo atualmente se encontra.
-int AlturaPilha(tp_pilha *p)
-{
-	return p->topo+1;
+int alturaPilha(tp_pilha *p){
+
+return p->topo+1;
 }
 
 #endif
